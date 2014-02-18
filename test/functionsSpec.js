98f88a7a2b03bb18819584ab6ce203a9d8dce6ff
Shane Osbourne <shane.osbourne8@gmail.com>
2014-02-18 08:47:12 +0000

    Clean for JSHINT, switch to gulp for build & ci, swith to mocha & chai for test, add example

--- test/functionsSpec.js
+++ test/functionsSpec.js
@@ -1,25 +1,28 @@
 var strings = require("../index.js");
-var compile = strings.getCompiler("{green:[BS]}");
-var chalk   = require('chalk');
+var chalk   = require("chalk");
 var stripColor = chalk.stripColor;
+var assert = require("chai").assert;
+
+// Compiler with prefx
+var compile = strings.getCompiler("{green:[BS]}");
 
 describe("Returning the compile function", function () {
-    it("can use a prefix", function () {
-        expect(typeof compile === "function").toBe(true);
+    it("can return a function", function () {
+        assert.isFunction(compile);
     });
     it("can compile with a prefix", function () {
         var actual = stripColor(compile("kittie"));
         var expected = "[BS] kittie";
-        expect(actual).toBe(expected);
+        assert.equal(actual, expected);
     });
     it("can compile multiple lines with prefix (1)", function () {
         var actual = stripColor(compile(["kittie", "shane"]));
         var expected = "[BS] kittie\n[BS] shane";
-        expect(actual).toBe(expected);
+        assert.equal(actual, expected);
     });
     it("can compile multiple lines with prefix (2)", function () {
         var actual = stripColor(compile(["{green:kittie}", "shane"]));
         var expected = "[BS] kittie\n[BS] shane";
-        expect(actual).toBe(expected);
+        assert.equal(actual, expected);
     });
 });
--- test/templateSpec.js
+++ test/templateSpec.js
@@ -1,48 +1,57 @@
 var compile = require("../index.js").compile;
-var chalk   = require('chalk');
+var chalk   = require("chalk");
 var stripColor = chalk.stripColor;
+var assert = require("chai").assert;
 
 describe("Compiling strings", function () {
 
     it("Can replace a single occurrence", function () {
         var string = "{green:Hi there}";
         var expected =  "Hi there";
-        expect(stripColor(compile(string))).toBe(expected);
+        var actual = stripColor(compile(string));
+        assert.equal(actual, expected);
     });
     it("Can replace a single occurrence", function () {
         var string = "{green:Hi :there}";
         var expected =  "Hi :there";
-        expect(stripColor(compile(string))).toBe(expected);
+        var actual = stripColor(compile(string));
+        assert.equal(actual, expected);
     });
     it("Can replace a multiple occurrences", function () {
-        var string = "{green:Hi there} {red:Shane}";
+        var string   = "{green:Hi there} {red:Shane}";
         var expected =  "Hi there Shane";
-        expect(stripColor(compile(string))).toBe(expected);
+        var actual   = stripColor(compile(string));
+        assert.equal(actual, expected);
     });
     it("Can replace a multiple occurrences with content between", function () {
         var string = "{green:Hi there} and then {red:Goodbye}";
         var expected =  "Hi there and then Goodbye";
-        expect(stripColor(compile(string))).toBe(expected);
+        var actual = stripColor(compile(string));
+        assert.equal(actual, expected);
     });
     it("can replace with regular content at the end", function () {
         var string = "{green:bet you can't compile} this";
         var expected =  "bet you can't compile this";
-        expect(stripColor(compile(string))).toBe(expected);
+        var actual = stripColor(compile(string));
+        assert.equal(actual, expected);
     });
     it("can replace with regular content at the begining", function () {
         var string = "bet you can't compile {green:this}";
         var expected =  "bet you can't compile this";
-        expect(stripColor(compile(string))).toBe(expected);
+        var actual = stripColor(compile(string));
+        assert.equal(actual, expected);
     });
     it("can replace with line breaks", function () {
         var string = "bet you can't \ncompile {green:this}";
         var expected =  "bet you can't \ncompile this";
-        expect(stripColor(compile(string))).toBe(expected);
+        var actual = stripColor(compile(string));
+        assert.equal(actual, expected);
     });
     it("can replace with random curlies", function () {
         var string = "bet you {can't}} \ncompile {green:this}";
         var expected =  "bet you {can't}} \ncompile this";
-        expect(stripColor(compile(string))).toBe(expected);
+        var actual = stripColor(compile(string));
+        assert.equal(actual, expected);
     });
 });
 
@@ -51,24 +60,28 @@ describe("Accepting variables to be interpolated", function () {
         var expected = "Hi there Shane";
         var template = "{red:Hi there} {:name:}";
         var params = {name: "Shane"};
-        expect(stripColor(compile(template, params))).toBe(expected);
+        var actual = stripColor(compile(template, params));
+        assert.equal(actual, expected);
     });
     it("can run through lodash templates (2)", function () {
         var expected = "Hi there Shane";
         var template = "{red:Hi there {:name:}}";
         var params = {name: "Shane"};
-        expect(stripColor(compile(template, params))).toBe(expected);
+        var actual = stripColor(compile(template, params));
+        assert.equal(actual, expected);
     });
     it("can run through lodash templates (3)", function () {
         var expected = "Hi there Shane\n";
         var template = "{red:Hi there {:name:}}\n";
         var params = {name: "Shane"};
-        expect(stripColor(compile(template, params))).toBe(expected);
+        var actual = stripColor(compile(template, params));
+        assert.equal(actual, expected);
     });
     it("can run through lodash templates (4)", function () {
         var expected = "Hi there\nShane\n";
         var template = "Hi there\n{red:Shane}\n";
         var params = {name: "Shane"};
-        expect(stripColor(compile(template, params))).toBe(expected);
+        var actual = stripColor(compile(template, params));
+        assert.equal(actual, expected);
     });
 });
